3fe148350a96cc5c46a73cdb406159ee7a40f381
Sharikathasni <Sharikarasheed114@gmail.com>
2024-09-18 16:26:48 +0400

    initial comit

---
+++ script.js
@@ -0,0 +1,15 @@
+const texts = document.querySelectorAll('.text');
+let index = 0;
+
+function showText() {
+    if (index < texts.length) {
+        texts[index].style.opacity = 1; // Show current text
+        index++;
+        setTimeout(() => {
+            texts[index - 1].style.opacity = 0; // Hide previous text
+            showText(); // Show next text
+        }, 4000); // Display each text for 2 seconds
+    }
+}
+
+document.addEventListener('DOMContentLoaded', showText);
